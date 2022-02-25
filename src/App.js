03a1776257e788eba147533c4b370f1dae384c3d
sagar <rebelsagarx@gmail.com>
2022-02-25 16:50:31 +0530

    First Lecture

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello </h1>
+     < User/>
+     < User/>
+    
+
+
+
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,10 @@
+export default function User() {
+    return (
+      <div className="App">
+       <h1>Hello Users component</h1>
+       <h2>component two</h2>
+
+      </div>
+    );
+  }
+ 
